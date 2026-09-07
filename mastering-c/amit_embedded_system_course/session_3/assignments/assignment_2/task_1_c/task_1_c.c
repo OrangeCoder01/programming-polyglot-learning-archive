b9ce3669fb98cd18b0a7b29931417141a9527c57
Yassin Diaa <yassindiaa06@gmail.com>
2026-09-07 17:47:52 +0300

    rewritting task_1 assignment_2 for confusion between prime and even number validation

--- mastering-c/amit_embedded_system_course/session_3/assignments/assignment_2/task_1_c/task_1_c.c
+++ mastering-c/amit_embedded_system_course/session_3/assignments/assignment_2/task_1_c/task_1_c.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 int main(void)
 {
-    int user_number = 0;
-    printf("The programs validate an input from user by only passing positive integer and a prime number\n");
+    int user_number = 0, i = 2, is_prime = 0x01;
+
+    printf("The programs validate an input from user by only passing positive integer and check if it is a prime number\n");
     printf("Please, enter the number: "); scanf("%d", &user_number);
-    while(!(user_number > 0 && user_number % 2 == 0))
+
+    while(!(user_number > 0))
     {
-        if(!(user_number > 0)){printf("Input: {%d} is not a positive number\n", user_number);}
-        if(!(user_number % 2 == 0)){printf("Input: {%d} is not a prime number\n", user_number);}
+        printf("Input: {%d} is not a positive number\n", user_number);
         printf("Enter the number again: "); scanf("%d", &user_number);
     }
-    printf("Number: %d is indeed a positive and a prime number", user_number);
+
+    for(i; i < user_number / 2; i++) /*i < user_number/2 takes less time than i < user_number condition*/
+    {
+        if (user_number % i == 0 ) 
+        {
+            is_prime = is_prime && (0); 
+            break;
+        }
+    }
+
+    if(is_prime){ printf("Input: {%d} is a prime number [Prime number]\n", user_number); }
+    else { printf("Input: {%d} is not a prime number [Not a prime number]\n", user_number); }
+
+
     return 0;
 }
