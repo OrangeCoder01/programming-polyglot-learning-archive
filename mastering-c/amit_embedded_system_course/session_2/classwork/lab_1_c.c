6248a79f8d359195914ce7d629d060b2794a1a83
Yassin Diaa <yassindiaa06@gmail.com>
2026-09-11 01:19:09 +0300

    polishing

--- mastering-c/amit_embedded_system_course/session_2/classwork/lab_1_c.c
+++ mastering-c/amit_embedded_system_course/session_2/classwork/lab_1_c.c
@@ -6,8 +6,16 @@ int main(void)
     // Setting (Set bit)
     int user_num = 0;
     printf("Please enter a number: ");scanf("%d", &user_num);
+
+    if(user_num < 0)
+    {
+        printf("Integer input can not be negative! ");
+        return 0;
+    }
+
     int bit = 0;
     printf("Please enter the bit: ");scanf("%d", &bit);
+    
     if(bit < 0)
     {
         printf("Bit order can not be negative! ");
